9245949ab443604f84735fe8bae9e4800bb76438
Retronyme <rubengres@hotmail.fr>
2020-03-28 23:51:42 +0100

    added supermarket

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool leftDown = false;
-    public bool rightDown = false;
-    public bool spaceDown = false;
-    public bool spacePressed = false;
+    public bool leftDown, rightDown, upDown, spaceDown, spacePressed, downDown = false;
 
     // Update is called once per frame
     void Update()
     {
         leftDown = Input.GetKey("left") | Input.GetKey("q") | Input.GetKey("a");
         rightDown = Input.GetKey("right") | Input.GetKey("d");
+        upDown = Input.GetKey("w") | Input.GetKey("z") | Input.GetKey("up");
+        downDown = Input.GetKey("s") | Input.GetKey("down");
         spaceDown = Input.GetKey("space");
         spacePressed = Input.GetKeyDown("space");
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,16 @@ using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float rollSpeed = 10f;
+    public float rollSpeed = 50f;
     public float maxspeed = 300f;
-    public float maxRotAngle = 2f;
+    public float maxRotAngle = 30f;
+    public float stoppingSpeed = 5f;
+    public float jumpForce = 100f;
 
     private float speed;
+    private bool rolling = false;
 
-    public Rigidbody rb;
+    private Rigidbody rb;
 
     private GameObject direction;
     private GameManager gm;
@@ -22,31 +25,42 @@ public class PlayerMovement : MonoBehaviour
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
         direction = GameObject.Find("Direction");
         dirScript = direction.GetComponent<DirectionScript>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
-        rotateRoll();
+        recalculateSpeed();
 
-        speed = new Vector2(rb.velocity.x, rb.velocity.z).magnitude;
-        if (gm.spacePressed) {
-            speed = speed + rollSpeed;
+        if(Math.Abs(rb.velocity.magnitude) < stoppingSpeed) {
+            Invoke("standUp", 1);      
+        } else {
+            rolling = true;
+            rotateRoll();
         }
-
-        speed = Math.Min(speed, maxspeed);
-
+        
         updateVelocity();
     }
 
-    void standUp() {
-        transform.localRotation = Quaternion.Euler(-90, 0, 0);
-        rb.velocity = Vector3.zero;
+    void recalculateSpeed() {
+        speed = new Vector2(rb.velocity.x, rb.velocity.z).magnitude * Math.Sign(speed);
+
+        if (gm.upDown)
+        {
+            speed = speed + (rollSpeed * Time.deltaTime);
+        }
+        else if (gm.downDown)
+        {
+            speed = speed - (rollSpeed * Time.deltaTime); ;
+        }
+        speed = Math.Max(Math.Min(speed, maxspeed), -maxspeed);
     }
 
-    void flipSide()
-    {
-        //place the roll according to the camera
-        transform.localRotation = Quaternion.Euler(0, direction.transform.rotation.eulerAngles.y + 90, 0);
+    void standUp() {
+        if (Math.Abs(rb.velocity.magnitude) < stoppingSpeed) {
+            transform.localRotation = Quaternion.Euler(-90, 0, 0);
+            rolling = false;
+        }
     }
 
     void updateVelocity() {
@@ -64,15 +78,21 @@ public class PlayerMovement : MonoBehaviour
 
     void rotateRoll()
     {
-        float x = 0;
+        float x = transform.eulerAngles.x;
         float y = direction.transform.rotation.eulerAngles.y + 90;
         float z = transform.eulerAngles.z;
+
+        if(x > maxRotAngle && x < 180) {
+            x = maxRotAngle;
+        } else if(x > 180 && x < 360 - maxRotAngle) {
+            x = 360 - maxRotAngle;
+        }
 
         Vector3 desiredRot = new Vector3(x,y,z);
         transform.rotation = Quaternion.Euler(desiredRot);
     }
 
     public bool isRolling() {
-        return true;
+        return rolling;
     }
 }
