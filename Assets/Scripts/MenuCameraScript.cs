2cdfa68556dd13ee49beebf50b27483dbd560290
Matthieu Deloffre <matthieu.techno@gmail.com>
2020-03-28 17:10:30 +0100

    added main menu canvas + camera

---
+++ Assets/Scripts/MenuCameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuCameraScript : MonoBehaviour
+{
+    public Transform target;
+    private float speedMod = 0.2f;
+    private Vector3 point;
+
+    void Start()
+    {
+        point = target.position;
+        transform.LookAt(point);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * speedMod);
+    }
+}
