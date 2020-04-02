2d98be3cd57d5079e9f6d2065da9bfd96cad851b
Retronyme <rubengres@hotmail.fr>
2020-04-03 00:01:22 +0200

    before merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,16 +9,25 @@ public class GameManager : MonoBehaviour
 
     public Text scoreText;
     private float timer;
+
+    public GameObject particles;
+    public GameObject zombie;
+
+    public int spawnTimer = 20;
+    private float stimer;
+
     private void Start()
     {
         scoreText.text = "Score : " + score.ToString("0");
+
+        processCollectables();
     }
 
     void Update()
     {
         updateKeys();
         addScore();
-
+        spawnZombie();
     }
 
     void addScore()
@@ -47,5 +56,27 @@ public class GameManager : MonoBehaviour
         downDown = Input.GetKey("s") | Input.GetKey("down");
         spaceDown = Input.GetKey("space");
         spacePressed = Input.GetKeyDown("space");
+    }
+
+    void processCollectables() {
+        GameObject[] cols = GameObject.FindGameObjectsWithTag("Collectable");
+        foreach (var col in cols)
+        {
+            Instantiate(particles, col.transform);
+        }
+
+        Destroy(particles);
+    }
+
+    void spawnZombie() {
+        stimer += Time.deltaTime;
+        if (stimer > spawnTimer)
+        {
+            GameObject[] spawns = GameObject.FindGameObjectsWithTag("Spawner");
+            int r = (int) Random.Range(0, spawns.Length);
+
+            Instantiate(zombie, spawns[r].transform);
+            stimer = 0;
+        }
     }
 }
